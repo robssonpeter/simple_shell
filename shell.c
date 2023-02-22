9e6e61edfb6509d5c4be856e69e5c273845c29c6
Ajiriwa Network <peterrobsson@gmail.com>
2023-02-22 22:13:10 +0000

    updating shell q1
    solved for question 1

--- shell.c
+++ shell.c
@@ -14,42 +14,44 @@
 int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
+	char *argv[] = { NULL };
+	char *envp[] = { NULL };
 	int status;
 
 	while (1) {
-		// Print prompt and read user input
-		printf("> ");
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-			// End of file condition (Ctrl+D)
-			printf("\n");
-			break;
+	// Print prompt and read user input
+	printf("> ");
+	if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+		// End of file condition (Ctrl+D)
+		printf("\n");
+		break;
+	}
+
+	// Remove newline character at the end of the command
+	command[strcspn(command, "\n")] = 0;
+
+	// Fork a new process to execute the command
+	pid_t pid = fork();
+	if (pid == -1) {
+		perror("fork");
+		continue;
+	} else if (pid == 0) {
+		// Child process
+		if (execve(command, argv, envp) == -1) {
+			// Error executing command
+			perror("execve");
+			exit(1);
 		}
-
-		// Remove newline character at the end of the command
-		command[strcspn(command, "\n")] = 0;
-
-		// Fork a new process to execute the command
-		pid_t pid = fork();
-		if (pid == -1) {
-			perror("fork");
-			continue;
-		} else if (pid == 0) {
-			// Child process
-			if (execlp(command, command, (char *) NULL) == -1) {
-				// Error executing command
-				perror("execlp");
-				exit(1);
+	} else {
+	    // Parent process
+		do {
+			// Wait for child process to finish
+			pid_t wpid = waitpid(pid, &status, WUNTRACED);
+			if (wpid == -1) {
+				perror("waitpid");
+				break;
 			}
-		} else {
-			// Parent process
-			do {
-				// Wait for child process to finish
-				pid_t wpid = waitpid(pid, &status, WUNTRACED);
-				if (wpid == -1) {
-					perror("waitpid");
-					break;
-				}
-			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 		}
 	}
 
