7ac72a5f30d5c735371ba64f9ba251ba96ecb162
Iku96 <ikumwana@gmail.com>
2023-02-22 18:59:21 -0800

    Implemented exit

--- shell.c
+++ shell.c
@@ -4,102 +4,68 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
-#include "main.h"
-#include "find_command.c"
 
-/**
- * main - Runs the shell
- * Return: 0 on success, non-zero on error
- */
-int main(void)
-{
-	char command[MAX_COMMAND_LENGTH];
-	char *argv[MAX_ARGS + 1]; /* +1 for NULL at the end */
-	char *envp[] = { NULL };
-	int status, argc;
-	char *arg, *path, *fullpath;
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARG_LENGTH 20
+#define MAX_ARGS 10
 
-	while (1)
-	{
-		/* Print prompt and read user input */
-		printf("$ ");
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-		{
-			/* End of file condition (Ctrl+D) */
-			printf("\n");
-			break;
-		}
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+    char *argv[MAX_ARGS + 1]; // +1 for NULL at the end
+    char *envp[] = { NULL };
+    int status;
 
-		/* Remove newline character at the end of the command */
-		command[strcspn(command, "\n")] = 0;
+    while (1) {
+        // Print prompt and read user input
+        printf("> ");
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+            // End of file condition (Ctrl+D)
+            printf("\n");
+            break;
+        }
 
-		/* Check for built-in commands */
-		if (strcmp(command, "exit") == 0)
-		{
-			break;
-		}
-		else if (strcmp(command, "env") == 0)
-		{
-			/* Print environment variables */
-			char **ep;
+        // Remove newline character at the end of the command
+        command[strcspn(command, "\n")] = 0;
 
-			for (ep = environ; *ep != NULL; ep++)
-			{
-				printf("%s\n", *ep);
-			}
-			continue;
-		}
+        // Check if command is "exit"
+        if (strcmp(command, "exit") == 0) {
+            exit(EXIT_SUCCESS);
+        }
 
-		/* Parse command line into arguments */
-		argc = 0;
-		arg = strtok(command, " ");
-		while (arg != NULL && argc < MAX_ARGS)
-		{
-			argv[argc++] = arg;
-			arg = strtok(NULL, " ");
-		}
-		argv[argc] = NULL;
+        // Parse command line into arguments
+        int argc = 0;
+        char *arg = strtok(command, " ");
+        while (arg != NULL && argc < MAX_ARGS) {
+            argv[argc++] = arg;
+            arg = strtok(NULL, " ");
+        }
+        argv[argc] = NULL;
 
-		/* Check if the command exists */
-		path = getenv("PATH");
-		fullpath = find_command(argv[0], path);
-		if (fullpath == NULL)
-		{
-			printf("Command not found\n");
-			continue;
-		}
+        // Fork a new process to execute the command
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            continue;
+        } else if (pid == 0) {
+            // Child process
+            if (execve(argv[0], argv, envp) == -1) {
+                // Error executing command
+                perror("execve");
+                exit(1);
+            }
+        } else {
+            // Parent process
+            do {
+                // Wait for child process to finish
+                pid_t wpid = waitpid(pid, &status, WUNTRACED);
+                if (wpid == -1) {
+                    perror("waitpid");
+                    break;
+                }
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    }
 
-		/* Fork a new process to execute the command */
-		pid_t pid = fork();
-
-		if (pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
-		else if (pid == 0)
-		{
-		/* Child process */
-		if (execve(fullpath, argv, envp) == -1)
-		{
-			/* Error executing command */
-			perror("execve");
-			exit(1);
-		}
-	}
-	else
-	{
-		do {
-			pid_t wpid = waitpid(pid, &status, WUNTRACED);
-
-			if (wpid == -1)
-			{
-				perror("waitpid");
-				break;
-			}
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	free(fullpath);
-	}
-	return (0);
+    return 0;
 }
+
